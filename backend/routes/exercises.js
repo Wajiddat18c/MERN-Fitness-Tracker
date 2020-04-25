1def932687388dd27bc32e7ad60fc90a70b4fa5d
Wajiddat18c <wajid0003@stud.kea.dk>
2020-04-25 13:12:50 +0200

    added CRUD

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -16,12 +16,12 @@ router.route("/add").post((req, res) => {
   const date = Date.parse(req.body.date);
 
   //ny instans af user
-  const newExercise = new Exercise({ 
-      username,
+  const newExercise = new Exercise({
+    username,
     description,
     duration,
-    date
-    });
+    date,
+  });
 
   //gemmer user
   newExercise
@@ -30,6 +30,34 @@ router.route("/add").post((req, res) => {
     .catch((err) => res.status(400).join("Error: " + err));
 });
 
+//get one id
+router.route("/:id").get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exercise) => res.json(exercise))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Exercise deleted."))
+    .catch((err) => res.status(400).json("ERROR: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exercise) => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+
+      exercise
+        .save()
+        .then(() => res.json("Exercise updated!"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error:" + err));
+});
 
 //route
 module.exports = router;
